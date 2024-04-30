cdb35ff3f745376b46759e4cf1e2f6d89c7e125c
farghaly223 <168374627+farghaly223@users.noreply.github.com>
2024-04-30 17:24:18 +0300

    Update women pants.js

--- js/women pants.js
+++ js/women pants.js
@@ -1,4 +1,4 @@
-var icon = document.getElementById("icon");
+var icon = document.getElementById("icon999");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
